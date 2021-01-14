7b4246f9cb0af7969ce604b1a15bc277c4197307
Fábio Kopezinski <fabiokopezinski@gmail.com>
2021-01-14 18:13:00 -0300

    feature:ajuste de projeto

--- src/main/java/br/com/api/pokedex/ApiPokedexApplication.java
+++ src/main/java/br/com/api/pokedex/ApiPokedexApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiPokedexApplication {
 
-	public static void main() {
-		SpringApplication.run(ApiPokedexApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPokedexApplication.class, args);
 	}
 
 }
